4540a97652e8303c75ca11e68fc42761f9c19eb8
Janith-Chamikara <janithchamikara13@gmail.com>
2024-10-29 11:22:45 +0530

    added today's session

---
+++ DSA/2024-10-29/LinkedList/Program.cs
@@ -0,0 +1,8 @@
+﻿using LinkedListNameSpace;
+
+LinkedList linkedList = new LinkedList();
+linkedList.AddFront(1);
+linkedList.AddFront(3);
+linkedList.Print();
+linkedList.AddFront(2);
+linkedList.Print();
