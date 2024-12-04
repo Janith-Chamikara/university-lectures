ceadd929166780f94732ea85e9658bbca76bdc63
Janith-Chamikara <janithchamikara13@gmail.com>
2024-12-04 19:06:15 +0530

    updated

---
+++ DSA/2024-11-26/sorting/selectionSort/Program.cs
@@ -0,0 +1,36 @@
+﻿class SelectionSort {
+    static void Sort(int [] array){
+        int N = array.Length;
+        
+        for(int i = 0;  i < N -1 ; i++){
+            int minIdx = i;
+
+            for( int j = i + 1; j < N; j++ ){
+                if(array[j] < array[minIdx]){
+                    minIdx = j;
+                }
+            }
+
+            int temp = array[i];
+            array[i] = array[minIdx];
+            array[minIdx] = temp;
+
+        }
+    }
+
+    static void PrintArray(int [] array){
+        Console.Write("[ ");
+        foreach(int val in array){
+            Console.Write($"{val} ,");
+        }
+        Console.Write(" ]");
+
+    }
+
+
+    public static void Main(string [] args){
+        int[] array =  {12, 34, 45, 2, 34, 34 };
+        Sort(array);
+        PrintArray(array);
+    }
+}
---
+++ GUI-programming/web-development/2024-12-03/main.js
@@ -0,0 +1,6 @@
+document.write("Hello World");
+alert("Hello");
+
+const submitButton = document.getElementById("submit-button");
+
+submitButton.addEventListener("click", () => alert("Say Hello!"));
